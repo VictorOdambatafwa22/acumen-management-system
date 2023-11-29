c9ec283b8a8ffc418472654b6e85ceadf7d1bf51
victor odambatafwa <omondivictoras@gmail.com>
2023-11-29 13:00:50 +0300

    Designed Owner component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar';
+import Owner from './Components/Owner';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div>
     <NavBar />
+
+    <Owner />
+    <Footer />
     {/* Add the rest of your content here */}
   </div>
   );
---
+++ client/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-red-300 text-white p-4">
+      <div className="container mx-auto text-center">
+        <p className="text-black">&copy; 2023 Shavic Enterprise Solutions. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
