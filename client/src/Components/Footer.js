649be43723a05de954c435d4b59d362c8f79e3d8
victor odambatafwa <omondivictoras@gmail.com>
2024-03-25 23:40:26 +0300

    Adjusted footer component

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-red-100 text-white p-4">
       <div className="container mx-auto text-center">
-        <p className="text-black">&copy; 2023 Shavic Enterprise Solutions. All rights reserved.</p>
+        <p className="text-black">&copy; 2023 Sanaa International Ltd. All rights reserved.</p>
       </div>
     </footer>
   );
